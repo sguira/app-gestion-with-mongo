ac74b7f713cd5b8c761615b1f35a68d81f309541
Sguira <sguira96@gmail.com>
2023-08-11 21:51:05 +0100

    rectification de l'erreur ajout produit

--- src/main/java/com/application/mongo/app_e_feray/controller/controller.java
+++ src/main/java/com/application/mongo/app_e_feray/controller/controller.java
@@ -530,7 +530,7 @@ public class controller {
             String remboursement = a.getDate() + a.getEspece();
             a.addAchat(remboursement);
             a.setNomFournisseur(f.getName());
-            a.getArticles_().add(val);
+            // a.setArticles_();;
             u.ajouter_achats(a_);
             usersR.save(u);
 
