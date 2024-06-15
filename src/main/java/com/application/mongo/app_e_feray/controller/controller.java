6325ee9930ba8f3f8aeeb3b5fbd4c929d5bd8b3d
sguira <stage_nlp3@smarte-conseil.com>
2024-06-15 23:18:10 +0100

    ajout vente

--- src/main/java/com/application/mongo/app_e_feray/controller/controller.java
+++ src/main/java/com/application/mongo/app_e_feray/controller/controller.java
@@ -298,7 +298,7 @@ public class controller {
         for (var d : date_) {
             double montantT = 0;
             for (var v : u.getVentes()) {
-                if (v.getDate().split(" ")[0].equals(d.split(date))) {
+                if (v.getDate().split(" ")[0].equals(d)) {
                     montantT += v.getEspece();
                 }
                 for (var e : v.getRemboursement()) {
