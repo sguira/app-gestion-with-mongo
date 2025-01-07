bd6b048307d894e57267aee92c7bcd7d199b7312
sguira <stage_nlp3@smarte-conseil.com>
2025-01-07 17:16:28 +0000

    changement de la strategie de generation de token

--- src/main/java/com/application/mongo/app_e_feray/services/JWTUtils.java
+++ src/main/java/com/application/mongo/app_e_feray/services/JWTUtils.java
@@ -1,8 +1,12 @@
 package com.application.mongo.app_e_feray.services;
 
+import java.security.AlgorithmConstraints;
 import java.security.Key;
 import java.util.Date;
 
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.Jwts;
@@ -12,25 +16,30 @@ import io.jsonwebtoken.security.Keys;
 @Component
 public class JWTUtils {
 
-    final private String SECRETKEY = "secretkey";
+    @Value("${app.secret-key}")
+    private String secreykey;
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    // private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     public String generateToken(String username) {
         return Jwts.builder()
                 .setSubject(username)
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(key)
+                .signWith(getSecretkey())
                 .compact();
     }
 
+    private Key getSecretkey() {
+        return new SecretKeySpec(secreykey.getBytes(), SignatureAlgorithm.HS256.getJcaName());
+    }
+
     public String extractUsername(String token) {
-        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(getSecretkey()).parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(key).parseClaimsJws(token);
+            Jwts.parser().setSigningKey(getSecretkey()).parseClaimsJws(token);
             return true;
         } catch (Exception e) {
             return false;
