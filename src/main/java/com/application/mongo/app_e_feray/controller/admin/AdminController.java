d19e53bb4fd41c4ffa9101f221b248a0aa5a822d
sguira <stage_nlp3@smarte-conseil.com>
2025-02-13 14:05:09 +0000

    ajout get user

--- src/main/java/com/application/mongo/app_e_feray/controller/admin/AdminController.java
+++ src/main/java/com/application/mongo/app_e_feray/controller/admin/AdminController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,6 +23,11 @@ public class AdminController {
     @GetMapping("/users")
     public ResponseEntity<Object> allUser() {
         return ResponseEntity.ok(userR.findAll());
+    }
+
+    @GetMapping(path = "/users/{id}")
+    ResponseEntity getUserDetail(@PathVariable("id") String id) {
+        return ResponseEntity.ok(userR.findById(id).orElse(null));
     }
 
 }
