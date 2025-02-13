b44db6ca8c05408c3be7fd60dcad4d624f3c0fc0
sguira <stage_nlp3@smarte-conseil.com>
2025-02-13 20:33:33 +0000

    ajout get user

--- src/main/java/com/application/mongo/app_e_feray/controller/admin/AdminController.java
+++ src/main/java/com/application/mongo/app_e_feray/controller/admin/AdminController.java
@@ -3,10 +3,12 @@ package com.application.mongo.app_e_feray.controller.admin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.application.mongo.app_e_feray.repository.UserRepositori;
+import com.application.mongo.app_e_feray.repository.VenteRepo;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AdminController {
 
     private final UserRepositori userR;
+    private final VenteRepo venteRepo;
 
     @GetMapping("/users")
     public ResponseEntity<Object> allUser() {
@@ -28,6 +31,16 @@ public class AdminController {
     @GetMapping(path = "/users/{id}")
     ResponseEntity getUserDetail(@PathVariable("id") String id) {
         return ResponseEntity.ok(userR.findById(id).orElse(null));
+    }
+
+    @DeleteMapping(path = "/delete-vente/{id}")
+    ResponseEntity delteVente(@PathVariable String id) {
+        try {
+            venteRepo.deleteById(id);
+            return ResponseEntity.ok("OK");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error");
+        }
     }
 
 }
